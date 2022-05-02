1b779b52af4b42fb259e6452f37e230d2462d31b
Matyáš Caras <contact@hernikplays.cz>
2022-05-02 12:10:06 +0200

    Odstranit nepouzity import

--- lib/okna/jidelnicek.dart
+++ lib/okna/jidelnicek.dart
@@ -9,7 +9,6 @@ import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-import '../loginmanager.dart';
 import '../main.dart';
 import 'about.dart';
 
